e3fd7d0cdf4f893dc43e38367e9729368820d38d
Meltice <noppanat.amp@student.mahidol.edu>
2020-12-02 23:33:29 +0000

    Remove Stop Words; Tokenize Data Version 1

--- Main.java
+++ Main.java
@@ -6,7 +6,12 @@ import org.jsoup.nodes.Document;
 class Main {
   public static void main(String[] args)  throws IOException {
     Sentiment rawText = new Sentiment();
-    
+
+    //Start text
+    System.out.format("Initializing code...\n\n\n");
+
+
+
     //pull text from link using jsoup
     Document doc = Jsoup.connect("https://www.metacritic.com/feature/reviews-for-the-sony-playstation-5-console-hardware").get();
     
@@ -16,12 +21,32 @@ class Main {
     //access a method from Clean to process the string
     rawText.removeStop(unprocessedText);
 
-    //print out string of char concatinated [Runs through String i which is process]
     /*
-    for (String index : rawText.wordList){
-      System.out.print(index);
+    for(String i : Sentiment.wordAL){
+      System.out.print(i +", ");
     }
-    
+    System.out.print("Total words : " + Sentiment.countWordAL);
     */
+
+
+    rawText.Rating();
+
+    System.out.println("Positive Sentiment: " + Sentiment.positive + "/" + Sentiment.countWordAL);
+    System.out.print("Negative Sentiment: " + Sentiment.negative + "/" + Sentiment.countWordAL);
+
+    System.out.println("Ratio: " + Sentiment.positive + "/" + Sentiment.countWordAL);
+
+
+    /*
+    for(String i : Sentiment.stopAL){
+      System.out.print(i +", ");
+    }
+    System.out.print("Total words : " + Sentiment.countStopAL);
+    */
+
+
+    //EndCode
+    System.out.format("\n\n\nCode deactivated...");
+
   }
 }
--- Sentiment.java
+++ Sentiment.java
@@ -1,60 +1,178 @@
-import java.util.*; 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 //Lets client object find amount of positive and negative sentiment
 public class Sentiment {
-  int positive, negative, ambiguous;
-  //New ArrayList word
+  static int countWordAL, countStopAL, positive, negative;
+  public int ambiguous;
+  //New ArrayList word Array List
+  static ArrayList<String> wordAL = new ArrayList();
+  //New ArrayList stop word Array List
+  static ArrayList<String> stopAL = new ArrayList();
 
+  static ArrayList<String> posAL = new ArrayList();
+  static ArrayList<String> negAL = new ArrayList();
+
+  
   //public method removing stop words
   public void removeStop(String text){
-    int wordCount;
+
+    //[CODE BELOW REMOVE ALL SPECIAL&NUM CHAR and ADD ALL WORDS PULLED FROM WEBSITE TO ARRAYLIST]
+
     //change stringed text into char and put them into to an array
     char[] charArray = text.toCharArray();
-    
-    /*
-    String charList = new String(charArray);       
 
-    //for every char in charList.Array [FAIL SAFE]
-  
-    for (int index = 0; index < charList.length - 1 ; index++){
-      if(charList.charAt(index) >= 0 && charList.charAt(index) <= 31){
-        charList.remove(index);
-      }
-      else if(charList.charAt(index) >= 33 && charList.charAt(index) <= 64){
-        charList.remove(index);
-      }
-    }
-  */
     //Take list of char and convert it into a string type
     String strList = new String(charArray);
  
     strList = strList.replaceAll("[^a-zA-Z]", " ");
-
+    
     //Take string w/ no speacial character (except WhiteSpaces) and shove it into a string array using split() with WhiteSpaces as the delimiter
     String[] wordArray = strList.split(" ");
     //[JAVA] the value of the local variable  wordList is not used
     
-    ArrayList<String> wordList = new ArrayList();
+    
 
+    //For string l add every word in Array wordArray into the Arraylist wordList
     for (String l : wordArray) {
-      wordList.add(l.toLowerCase());
+      wordAL.add(l.toLowerCase());
     }
 
     //Remove blank string value until there is no more blank value to remove     
-    while (wordList.remove("")){
+    while (wordAL.remove("")){
+    }
+
+    //CheckPoint* Iteration 1 complete = removed special characters between text and add words into Array, ready for comparison
+
+    //[CODE BELOW ADD ELEMENT TO ARRAYLIST INSERTED FROM STOP.TXT FILE]
+    
+    //initialize blank local string variable
+    String stopText = "";
+    
+    //Try pulling file from stop.txt
+    try {
+      //Access stopwords from stop.text using java.io.File
+      File myFile = new File("stop.txt");
+      //New Scanner myScan
       
+      Scanner myScan = new Scanner(myFile);
+
+      while(myScan.hasNextLine()) {
+        stopText += myScan.nextLine() + " ";
+      }
+      myScan.close();
+    } catch (FileNotFoundException e){
+      System.out.println("Error Occurred");
+      e.printStackTrace();
     }
 
+    String[] stopArray = stopText.split(",");
 
-    for (String s : wordList) {
-      System.out.print(s + ",");
+    
+    for (String a : stopArray) {
+      stopAL.add(a.toLowerCase());
+    }
+    
+    //CheckPoint* Iteration 2 complete = access txtfile and add it into array...  => compare with wordAL to remove stop words
+    this.countStopAL = stopAL.size();
+    
+    //[CODE BELOW COMPARES ARRAYS WORDAL AND STOPAL AND REMOVE STOP WORD FROM THE WEBSITE ]
+
+    //Checkpoint* Iteration 3 complete = removed stop text from array => 
+    for (int b = wordAL.size()-1; b > 0; b--){
+      for(int c = 0; c < stopAL.size() - 1; c++){
+        if(wordAL.get(b).equals(stopAL.get(c))){
+          wordAL.remove(b);
+        }
+      }
+    }
+
+    this.countWordAL = wordAL.size();
+  }
+
+
+
+  public void Rating(){
+    String posText = "";
+    String negText = "";
+    
+    //Try pulling file from pos.txt
+    try {
+      //Access negativeword from pos.text using java.io.File
+      File posFile = new File("pos.txt");
+      
+      //New Scanner PosScanner
+      Scanner myPosScan = new Scanner(posFile);
+
+      while(myPosScan.hasNextLine()) {
+        posText += myPosScan.nextLine() + " ";
+      }
+      
+      myPosScan.close();
+
+    } catch (FileNotFoundException pos){
+      System.out.println("Error Occurred at PosPrintError");
+      pos.printStackTrace();
+    }
+
+    //Try pulling file from neg.txt
+    try {
+      //Access neg from neg.text using java.io.File
+      File negFile = new File("neg.txt");
+      //New Scanner myScan
+      
+      Scanner myNegScan = new Scanner(negFile);
+
+      while(myNegScan.hasNextLine()) {
+        negText += myNegScan.nextLine() + " ";
+      }
+
+      myNegScan.close();
+
+    } catch (FileNotFoundException neg){
+      System.out.println("Error Occurred at NegPrintError");
+      neg.printStackTrace();
     }
     
     
-    wordList.remove(1);
-    System.out.print(wordList.size());
+    String[] posArray = posText.split(",");
+
+    String[] negArray = negText.split(",");
+    
+    
+    for (String pos : posArray) {
+      posAL.add(pos.toLowerCase());
+    } 
+    for (String neg : negArray) {
+      negAL.add(neg.toLowerCase());
+    }
+
+    int ratePositive = 0;
+    int rateNegative = 0;
+
+    for (int x = wordAL.size()-1; x > 0; x--){
+      for(int y = 0; y < posAL.size() - 1; y++){
+        if(wordAL.get(x).equals(posAL.get(y))){
+          ratePositive++;
+        }
+      }
+    }
+
+    
+    for (int x = wordAL.size()-1; x > 0; x--){
+      for(int y = 0; y < posAL.size() - 1; y++){
+        if(wordAL.get(x).equals(negAL.get(y))){
+          rateNegative++;
+        }
+      }
+    }
+    
+    this.negative = rateNegative;
+    this.positive = ratePositive;
+
   }
 }
