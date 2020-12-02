d5232b665dffd916173b7e8f93b9ab5096a1587b
Meltice <noppanat.amp@student.mahidol.edu>
2020-12-02 01:00:46 +0000

    ArrayLower

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
-import java.util.ArrayList; 
-import java.io.IOException;  
-import org.jsoup.Jsoup;  
-import org.jsoup.nodes.Document; 
+import java.util.ArrayList;
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 class Main {
   public static void main(String[] args)  throws IOException {
-    Clean rawText = new Clean();
+    Sentiment rawText = new Sentiment();
     
     //pull text from link using jsoup
     Document doc = Jsoup.connect("https://www.metacritic.com/feature/reviews-for-the-sony-playstation-5-console-hardware").get();
@@ -15,13 +15,13 @@ class Main {
 
     //access a method from Clean to process the string
     rawText.removeStop(unprocessedText);
-    
 
     //print out string of char concatinated [Runs through String i which is process]
-    for (String i : rawText.words){
-      System.out.print(i);
+    /*
+    for (String index : rawText.wordList){
+      System.out.print(index);
     }
     
-
+    */
   }
 }
--- Sentiment.java
+++ Sentiment.java
@@ -1,39 +1,60 @@
+import java.util.*; 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 //Lets client object find amount of positive and negative sentiment
 public class Sentiment {
   int positive, negative, ambiguous;
   //New ArrayList word
-  ArrayList<String> words = new ArrayList();
-
-  ArrayList<String> Wantedword = new ArrayList();
 
   //public method removing stop words
-  public void removeStop (String text) {
-    
+  public void removeStop(String text){
+    int wordCount;
     //change stringed text into char and put them into to an array
-    char[] charList = text.toCharArray();
-    //String[] StringList = {" ", " ", " ", " ", " "};
-    String currentWord = "";
+    char[] charArray = text.toCharArray();
+    
+    /*
+    String charList = new String(charArray);       
 
-    //for every char in charList.Array 
-    for (int index = 0; index < charList.length; index++) {
-      if (charList[index] == ' '){
-
-        if (!currentWord.equals("")){
-
-          words.add(currentWord.toLowerCase());
-        }
-        currentWord = "";
-
-      }else{
-
-        if ((charList[index] <= 122 && charList[index] >= 97) || (charList[index] <= 90 && charList[index] >= 65)) {
-
-          currentWord += charList[index];
-
-        }
+    //for every char in charList.Array [FAIL SAFE]
+  
+    for (int index = 0; index < charList.length - 1 ; index++){
+      if(charList.charAt(index) >= 0 && charList.charAt(index) <= 31){
+        charList.remove(index);
+      }
+      else if(charList.charAt(index) >= 33 && charList.charAt(index) <= 64){
+        charList.remove(index);
       }
     }
+  */
+    //Take list of char and convert it into a string type
+    String strList = new String(charArray);
+ 
+    strList = strList.replaceAll("[^a-zA-Z]", " ");
+
+    //Take string w/ no speacial character (except WhiteSpaces) and shove it into a string array using split() with WhiteSpaces as the delimiter
+    String[] wordArray = strList.split(" ");
+    //[JAVA] the value of the local variable  wordList is not used
+    
+    ArrayList<String> wordList = new ArrayList();
+
+    for (String l : wordArray) {
+      wordList.add(l.toLowerCase());
+    }
+
+    //Remove blank string value until there is no more blank value to remove     
+    while (wordList.remove("")){
+      
+    }
+
+
+    for (String s : wordList) {
+      System.out.print(s + ",");
+    }
+    
+    
+    wordList.remove(1);
+    System.out.print(wordList.size());
   }
 }
